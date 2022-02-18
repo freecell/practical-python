b20d28e4f598f85c6a0fc5d7a79ecb7b5f0ea809
freecell <isharma92@gmail.com>
2022-02-18 16:30:23 +0000

    exercises completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+cost = 0
+
+with open(r'C:\Users\Ohwen\py_projects\learning\practical-python\Work\Data\portfolio.csv', 'rt') as f:
+    headers = next(f).split(',')
+    for line in f:
+        row = line.split(',')
+        #print (row[0], row[1], row[2])
+        cost = cost + (int(row[1]) * float(row[2]))
+    
+    print(f'Total cost {round(cost, 2)}')
+
+    
